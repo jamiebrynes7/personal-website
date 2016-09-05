6327f33f55672733e3d80e5099e2c8c48c7ce0fa
Jamie Brynes <jamiebrynes7@gmail.com>
2016-09-05 01:44:48 -0700

    chg: Add scripts for retrieving and formatting WP posts

---
+++ js/blogposts.js
@@ -0,0 +1,3 @@
+$.get('../php/wp-latest-blog-carousel.php', function(data) {
+  $('#blogList').html(data)
+});
---
+++ php/wp-latest-blog-carousel.php
@@ -0,0 +1,29 @@
+
+<?php
+  /**
+  * Retrieve Wordpress latest blog posts.
+  * @package Wordpress
+  */
+
+  require('../blog/wp-load.php');
+
+  //Load the recent top 10 posts
+  query_posts('posts_per_page=3');
+ ?>
+
+<?php if (have_posts()) : ?>
+  <?php while(have_posts()) : the_post(); ?>
+    <div class="blog-post">
+      <div class="row">
+        <div class="col-xs-8">
+          <p class="blog-post-title"><?php the_title(); ?></p>
+        </div>
+        <div class="col-xs-4">
+          <p class="text-right text-italics"><?php the_time('F jS'); ?>, <?php the_time('Y'); ?></p>
+        </div>
+      </div>
+      <?php the_post_thumbnail(); ?>
+      <p class="blog-post-content"><?php the_excerpt(); ?></p>
+    </div>
+  <?php endwhile; ?>
+<?php endif ?>
