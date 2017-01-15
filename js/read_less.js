63fe078c355641a9e8abb6343feed6b354049fca
Jamie Brynes <jamiebrynes7@gmail.com>
2017-01-15 16:02:17 +0000

    chg: Convert project to MDB

--- js/read_less.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-
-  $('div.read_less_btn').click(function(event){
-
-    event.preventDefault();
-    $(this).parents('.project').find('.more_text').hide();
-    $(this).parents('.project').find('.read_more_btn').show();
-  })
-});
